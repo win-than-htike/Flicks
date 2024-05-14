1e4ec337885e44f4493ecd632884b4fce7f93b46
Win Than Htike <winthan.htike@7peakssoftware.com>
2024-05-14 21:13:50 +0700

    Crafting Class

--- flicks.rb
+++ flicks.rb
@@ -1,11 +1,37 @@
-def movie_info(title:, rank: title.length)
-    stars = "⭐" * rank
-    "#{title} has a rank of #{rank}: #{stars}"
+class Movie
+    def initialize(title, rank = 5)
+        @title = title.capitalize
+        @rank = rank
+    end
+
+    def thumbs_up
+        @rank += 1
+    end
+
+    def thumbs_down
+        @rank -= 1
+    end
+
+    def to_s
+        "#{@title} has a rank of #{@rank}"
+    end
 end
 
-puts movie_info(rank: 10, title: "gonnies")
-# puts movie_info("ghostbusters", 9)
+movie_1 = Movie.new("goonies", 10)
+puts movie_1
+movie_1.thumbs_up
+puts movie_1
+movie_1.thumbs_up
+puts movie_1
 
-# movie_title = "goldfinger"
-# movie_rank = 8
-# puts movie_info(movie_title)
+movie_2 = Movie.new("ghostbusters", 9)
+puts movie_2
+movie_2.thumbs_down
+puts movie_2
+
+movie_3 = Movie.new("holdfinger")
+puts movie_3
+
+puts 3.to_s
+puts "hello".to_s
+puts movie_1
