a66bfe48aabec7267673ad2675cceb3324413b8c
Win Than Htike <winthan.htike@7peakssoftware.com>
2024-05-28 21:47:12 +0700

    Attr is For Attribute

--- studio_game.rb
+++ studio_game.rb
@@ -1,12 +1,52 @@
-puts "Let's play a game!"
-puts "🚀" * 25
+class Player
 
-puts "\t3\n\t2\n\t1"
+    attr_reader :health, :score
+    attr_accessor :name
 
-name = "finn"
-health = 60
+    def initialize(name, health = 100)
+        @name = name
+        @health = health
+        @score = name.length + health
+    end
 
-formatted_name = name.capitalize.ljust(30, ".")
+    def name=(new_name)
+        @name = new_name.capitalize
+    end
 
-puts "#{name.capitalize.strip}'s health is #{health.to_s.reverse.to_i.pow(2)}"
-puts "#{formatted_name} #{health} health"
+    def boost
+        @health += 15
+    end
+
+    def drain
+        @health -= 10
+    end
+
+    def to_s
+        "I'm #{@name} with a health of #{@health} and a score of #{@score}"
+    end
+
+end
+
+player_1 = Player.new("finn", 60)
+puts player_1
+player_1.boost
+puts player_1
+
+player_2 = Player.new("lucy", 90)
+puts player_2
+player_2.drain
+puts player_2
+
+player_3 = Player.new("jase")
+puts player_3
+player_3.boost
+player_3.boost
+puts player_3
+
+player_4 = Player.new("alex", 125)
+puts player_4.name
+player_4.name = "alexander"
+puts player_4.name
+puts player_4.health
+puts player_4.score
+puts player_4
