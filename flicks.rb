828f330d82c3cf8caa06ba7e4d3b98d53cfa0782
Win Than Htike <winthan.htike@7peakssoftware.com>
2024-12-16 20:06:47 +0700

    Array Party

--- flicks.rb
+++ flicks.rb
@@ -1,4 +1,8 @@
 class Movie
+
+    attr_accessor :title
+    attr_reader :rank
+
     def initialize(title, rank = 5)
         @title = title.capitalize
         @rank = rank
@@ -18,20 +22,57 @@ class Movie
 end
 
 movie_1 = Movie.new("goonies", 10)
-puts movie_1
-movie_1.thumbs_up
-puts movie_1
-movie_1.thumbs_up
-puts movie_1
-
 movie_2 = Movie.new("ghostbusters", 9)
-puts movie_2
-movie_2.thumbs_down
-puts movie_2
+movie_3 = Movie.new("goldfinger")
 
-movie_3 = Movie.new("holdfinger")
-puts movie_3
+movies = [movie_1, movie_2, movie_3]
 
-puts 3.to_s
-puts "hello".to_s
-puts movie_1
+puts "\nBefore Watching!"
+puts movies
+
+movies.each do |movie|
+    number_rolled = rand(1..6)
+
+    case number_rolled
+    when 1..2
+        movie.thumbs_down
+        puts "#{movie.title} got a 👎"
+    when 3..4
+        puts "#{movie.title} got skipped"
+    else
+        movie.thumbs_up
+        puts "#{movie.title} got a 👍"
+    end
+end
+
+puts "\nAfter Watching!"
+puts movies
+
+## Checking with if else
+# if number_rolled < 3
+#     movie_1.thumbs_down
+#     puts "#{movie_1.title} got a 👎"
+# elsif number_rolled < 5
+#     puts "#{movie_1.title} got skipped"
+# else
+#     movie_1.thumbs_up
+#     puts "#{movie_1.title} got a 👍"
+# end
+
+# puts movie_1
+# movie_1.thumbs_up
+# puts movie_1
+# movie_1.thumbs_up
+# puts movie_1
+
+# movie_2 = Movie.new("ghostbusters", 9)
+# puts movie_2
+# movie_2.thumbs_down
+# puts movie_2
+
+# movie_3 = Movie.new("holdfinger")
+# puts movie_3
+
+# puts 3.to_s
+# puts "hello".to_s
+# puts movie_1
