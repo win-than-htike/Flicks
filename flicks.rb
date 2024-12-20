5c920a695055ec8c086b0337d144ffd8d57df135
Win Than Htike <winthan.htike@7peakssoftware.com>
2024-12-20 12:03:12 +0700

    File Ins and Outs

--- flicks.rb
+++ flicks.rb
@@ -9,16 +9,31 @@ playlist_1 = Playlist.new("Kermit")
 playlist_1.add_movie(movie_1)
 playlist_1.add_movie(movie_2)
 playlist_1.add_movie(movie_3)
-playlist_1.play(2)
+
+loop do
+    print "\nHow many viewings? ('quit' to exit) "
+
+    answer = gets.chomp.downcase
+    case answer
+        when /^\d+$/
+            playlist_1.play(answer.to_i)
+        when "quit", "exit"
+            playlist_1.print_stats
+            break
+        else
+            puts "Please enter a number or 'quit'"
+    end
+end
+
 playlist_1.print_stats
 
-playlist_2 = Playlist.new("Fozzie")
-playlist_2.add_movie(movie_3)
+# playlist_2 = Playlist.new("Fozzie")
+# playlist_2.add_movie(movie_3)
 
-movie_4 = Movie.new("gremlins", 7)
-playlist_2.add_movie(movie_4)
-playlist_2.play(3)
-playlist_2.print_stats
+# movie_4 = Movie.new("gremlins", 7)
+# playlist_2.add_movie(movie_4)
+# playlist_2.play(3)
+# playlist_2.print_stats
 
 ## Checking with if else
 # if number_rolled < 3
